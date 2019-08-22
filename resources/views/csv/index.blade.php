971a6979996baa097f1bcf7365bdd174d9148d3b
tanaka <tanaka@logical-studio.com>
2019-08-22 09:01:25 +0900

    /csvにタイトル追加

--- resources/views/csv/index.blade.php
+++ resources/views/csv/index.blade.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+{{--    <meta name="csrf-token" content="{{ csrf_token() }}">--}}
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+</head>
+
+<body>
 {{-- Todo: tanaka bootstrapを使用するのが練習にもなるし、レスポンシブにもなってよいのでは？？ --}}
 <form method="post" action="{{ route('csv::post') }}" enctype="multipart/form-data" id="csvUpload">
     {{ csrf_field() }}
@@ -50,6 +67,8 @@
         </div>
     </div>
 </form>
+</body>
+</html>
 
 {{-- jqueryの呼び出し --}}
 <script
